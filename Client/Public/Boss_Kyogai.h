d1d3701085287247c88b70b95fd95ce8726e369f
OnepunchKing1 <titan5712@naver.com>
2023-08-14 17:44:56 +0900

    아카자 애니메이션 정리

--- Client/Public/Boss_Kyogai.h
+++ Client/Public/Boss_Kyogai.h
@@ -38,12 +38,28 @@ public:
 		ANIM_STEP_RIGHT = 50, // 우대시
 		ANIM_STEP_RIGHT2 = 51, // 우점프
 
+		ANIM_WALK_LEFT_START = 60, // 좌 걷기
+		ANIM_WALK_LEFT_LOOP = 61, // 좌 걸어
+		ANIM_WALK_LEFT_END = 62, // 좌 걸어 끝
+		ANIM_WALK_RIGHT_START = 63, // 우 걷기
+		ANIM_WALK_RIGHT_LOOP = 61, // 좌 걸어
+		ANIM_WALK_RIGHT_END = 62, // 좌 걸어 끝
 		
-
 		////////////////// 스킬 ///////////////////////
 		ANIM_ATKCMB1 = 1, // 배꼽에 있는 북 3연방 -> 톱날 세번나오는듯?
 		ANIM_ATKCMB2 = 2, // 두방침
 		ANIM_ATKCMB3 = 3, // 라스트
+
+		//페이즈 2 배꼽 북 연속치기
+		ANIM_ATKCMB_01READY = 4, // 연속 배꼽치기 준비자세
+		ANIM_ATKCMB_02LOOP = 5, // 배꼽 연속으로 치기
+		ANIM_ATKCMB_03END = 6, // 배꼽 연속치기 마무리 오른손 아이들자세로 돌아옴
+
+		ANIM_ATKSKCMB = 8, // 으으 기모으고 배꼽치기 와다다다닥
+		ANIM_ATKPUNCH = 9, // 강펀치~
+
+		ANIM_KICKDOWN = 11, // 왼발 내려찍기
+		ANIM_STOMPKICK = 13, // 내려찍기 준비자세하고 함 이거쓰자
 
 		ANIM_ATKSK_READY = 18, // 갈매기 레디
 		ANIM_ATKSK_LOOP = 19, // 레디 루프
@@ -51,14 +67,35 @@ public:
 		ANIM_ATKSK_RB = 21, // 오른쪽 엉덩이 빵
 		ANIM_ATKSK_LF = 22, // 왼쪽 가슴
 		ANIM_ATKSK_RF = 23, // 오른쪽 가슴
+		ANIM_ATKSK_END = 24, // 끝나고 42 IDLE로 가야함
 
-		//ANIM_ATKSK_
+		ANIM_ROOMCHANGE_READY = 25, // 만세 시작 북와다다다다다ㅏ 준비자세
+		ANIM_ROOMCHANGE_START = 26, // 북 와다다다다다ㅏ 방바꾸고스킬 마구마구 씀
+		ANIM_ROOMCHANGE_END = 27, // 북 와다다다다다 끝남
+
+		ANIM_ATKSTEP_BACK = 28, // 뒤돌면서 북치기
+		ANIM_ATKSTEP_FRONT = 29, // 앞돌면서 북치기
+
+		
 		////////////////// 히트모션 ///////////////////
+		ANIM_GUARD_READY = 32, // 가드 준비자세
+		ANIM_GUARD_LOOP = 33, // 가드 루프
+		ANIM_GUARD_END = 34, // 가드 풀었다
+		ANIM_GUARD_BREAK = 36, // 가드 풀림
 
-	
-
+		ANIM_BASETURN_L = 52, // 왼쪽 어깨 살짝 히트모션 아닌데 쓸만할듯?
+		ANIM_BASETURN_R = 53, // 오른쪽 어깨 살짝
+		
 		////////////////// 엔드모션 ///////////////////
 		ANIM_HEAL,
+		ANIM_DEATH = 66, // 데스
+		ANIM_DEATH_GETUP = 69, // 죽음에서 살아나기
+		ANIM_HIT = 67,
+		ANIM_GETUP = 68, // 일어나기
+		ANIM_HITDOWN = 70, // 무릎꿇으면서 앉음
+		ANIM_HITDOWN_LOOP = 71, // 무릎꿇는 자세 루프
+		ANIM_HITDOWN_GETUP = 72, // 머리털고 일어남
+		ANIM_HITDOWN_RETURN = 31, // 연결
 
 		////////////////// 컷신모션 ///////////////////
 		ANIM_AWAKE = 0 // 개방신
